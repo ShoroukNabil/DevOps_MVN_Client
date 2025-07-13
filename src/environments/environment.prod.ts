ca17fd950b4ba150d6f3611aa635078af6e033c0
snabil <snabil@kn-it.com>
2025-07-13 15:10:17 +0300

    Fix backend service name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://springboot-service:8040'
-  apiUrl: 'http://ec2-3-92-62-45.compute-1.amazonaws.com:8040'
+  apiUrl: 'http://devops-springboot-backend:8040'
 };
