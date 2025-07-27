4e67a937f52bd0dad7aa072ac7202c3419571a03
ShoroukNabil <38180573+ShoroukNabil@users.noreply.github.com>
2025-07-27 17:50:42 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://springboot-service:8040'
-  apiUrl: 'http://ec2-3-92-62-45.compute-1.amazonaws.com:8040'
+  apiUrl: 'http://localhost:8040'
 };
